22c080ec888045dea82fe950a01bff50bbe5b05a
Steph Crown <emmanuelstephen024@gmail.com>
2025-12-09 19:23:17 +0100

    Create twelve days in christmas function

--- src/main.rs
+++ src/main.rs
@@ -12,6 +12,8 @@ fn main() {
     let fib_val_rec = get_fib_recur(num);
     println!("zeroth number in Fib sequence is loop {fib_val}");
     println!("zeroth number in Fib sequence is rec {fib_val_rec}");
+
+    twelve_days_christmas(12);
 }
 
 fn fah_to_cel(val: f32) -> f32 {
@@ -45,4 +47,40 @@ fn get_fib_recur(n: u128) -> u128 {
     }
 
     return get_fib_recur(n - 1) + get_fib_recur(n - 2);
+}
+
+fn twelve_days_christmas(n: usize) {
+    if n > 12 || n < 1 {
+        println!("Error!. Must be between 1 and 12");
+        return;
+    }
+
+    const GIFTS: [&str; 12] = [
+        "A partridge in a pear tree",
+        "Two turtle doves and",
+        "Three french hens",
+        "Four calling birds",
+        "Five golden rings",
+        "Six geese a-laying",
+        "Seven swans a-swimming",
+        "Eight maids a-milking",
+        "Nine ladies dancing",
+        "Ten lords a-leaping",
+        "Eleven pipers piping",
+        "Twelve drummers drumming",
+    ];
+
+    const ORD: [&str; 12] = [
+        "first", "second", "third", "fourth", "fifth", "sixth", "seventh", "eight", "ninth",
+        "tenth", "eleventh", "twelvth",
+    ];
+
+    let ord_string = ORD[n - 1];
+
+    println!("On the {ord_string} day of Christmas, my true love sent to me");
+
+    for num in (0..n).rev() {
+        let gift = GIFTS[num];
+        println!("{gift}")
+    }
 }
