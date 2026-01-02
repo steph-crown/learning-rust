1eda60215d4a260abc8d1e12cf85abab54de464b
Steph Crown <emmanuelstephen024@gmail.com>
2026-01-02 12:44:22 +0100

    Learned about lifetimes

---
+++ src/lifetimes.rs
@@ -0,0 +1,13 @@
+pub fn run() {
+  let string1 = String::from("long string is long");
+  let result;
+  {
+    let string2 = String::from("xyz");
+    result = longest(&string1, &string2);
+    println!("The longest string is {result}");
+  }
+}
+
+fn longest<'a>(x: &'a String, y: &'a String) -> &'a String {
+  if x.len() > y.len() { x } else { y }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,13 @@
 // mod collections;
 // mod employees;
 // mod pig_latin;
-mod generics;
+// mod generics;
+mod lifetimes;
 
 fn main() {
   // crate::collections::run();
   // crate::pig_latin::run();
   // crate::employees::run();
-  crate::generics::run();
+  // crate::generics::run();
+  crate::lifetimes::run();
 }
